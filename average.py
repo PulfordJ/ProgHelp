a53d5dbe2c7ce24caf9abd7b59efb1f3a2801d50
John Pulford <johnpulford@gmail.com>
2014-10-22 22:56:08 +0100

    Average with min and max

--- average.py
+++ average.py
@@ -13,11 +13,17 @@ print("Use a negative number to end")
 
 # get first mark
 mark = float(input("Mark: "))
+maxi = mark
+mini = mark
 
 # process mark and get next one
 # continue until negative number entered
 while mark >= 0 :
     total = total + mark
+    if (mark > maxi):
+        maxi = mark
+    if (mark < mini):
+        mini = mark
     count = count + 1
     mark = float(input("Mark: "))
 
@@ -26,3 +32,6 @@ if count == 0 :
    print("No marks entered")
 else :
    print("The average mark is", round(total/count, 2))
+
+print("The max mark is", maxi)
+print("The min mark is", mini)
